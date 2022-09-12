0691d8d406a389b4bf4aa89e5dda7a82d7ac7bc5
Juan Felipe Huan Lew Yee <felipe.lew.yee@gmail.com>
2022-09-11 22:48:47 -0500

    Added incrmental functions for C and gamma

--- src/guess.jl
+++ src/guess.jl
@@ -41,3 +41,36 @@ function read_all(;title = "donof")
 
 end
 
+function increment_C(old_C,old_ncwo,p)
+
+    C = zeros(p.nbf,p.nbf)
+    C[1:p.nbf,1:p.no1+p.ndns] = old_C[1:p.nbf,1:p.no1+p.ndns]
+    for i in 1:p.ndoc
+        old_ll = p.no1 + p.ndns + old_ncwo*(p.ndoc - i) + 1
+        old_ul = p.no1 + p.ndns + old_ncwo*(p.ndoc - i + 1)
+        ll = p.no1 + p.ndns + p.ncwo*(p.ndoc - i) + 1
+        ul = p.no1 + p.ndns + p.ncwo*(p.ndoc - i + 1)
+	dif_ncwo = p.ncwo - old_ncwo
+        C[1:p.nbf,ll:ll+old_ncwo-1] = old_C[1:p.nbf,old_ll:old_ul]
+	C[1:p.nbf,ll+old_ncwo:ul] = old_C[1:p.nbf,p.no1+p.ndns+old_ncwo*p.ndoc+dif_ncwo*(i-1)+1:p.no1+p.ndns+old_ncwo*p.ndoc+dif_ncwo*i]
+    end
+    C[1:p.nbf,p.no1+p.ndns+p.ndoc*p.ncwo+1:p.nbf] = old_C[1:p.nbf,p.no1+p.ndns+p.ndoc*p.ncwo+1:p.nbf]
+
+    return C
+
+end
+
+function increment_gamma(old_gamma,old_ncwo,p)
+    
+    gamma = ones(p.nv)*pi/2
+    for i in 1:p.ndoc
+        gamma[i] = old_gamma[i]
+	old_ll = p.ndoc + (old_ncwo-1)*(i-1) + 1
+	old_ul = p.ndoc + (old_ncwo-1)*i
+	ll = p.ndoc + (p.ncwo-1)*(i - 1) + 1
+	ul = p.ndoc + (p.ncwo-1)*i
+	gamma[ll:ll+(old_ncwo-1)-1] = old_gamma[old_ll:old_ul]
+    end
+    
+    return gamma
+end
