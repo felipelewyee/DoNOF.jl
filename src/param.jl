e109e8ec630589947e7ed7933fa724062055438a
Juan Felipe Huan Lew Yee <felipelew@gmail.com>
2022-09-20 13:35:47 -0500

    Modified autozeros

--- src/param.jl
+++ src/param.jl
@@ -216,12 +216,12 @@ function autozeros(p;restart=false)
 	else
 	    p.nzeros = abs(trunc(Int,log10(p.threshl))) - 1
             p.nzerosr = p.nzeros
-            p.nzerosm = abs(int(log10(p.threshl))) + 2
+            p.nzerosm = abs(trunc(Int,log10(p.threshl))) + 2
 	end
     else
         p.nzeros = 1
         p.nzerosr = 2
-        p.nzerosm = abs(int(log10(p.threshl))) + 2
+        p.nzerosm = abs(trunc(Int,log10(p.threshl))) + 2
     end
 end
 
